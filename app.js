4c82b44d7e611ea159e6998484db2fa01acddab9
lin <lin@lindeAir-2.lan>
2019-03-23 17:05:03 +0800

    将input 输入的值 写入 txt 文件中.

--- app.js
+++ app.js
@@ -15,10 +15,20 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+    req.on("data", chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, err => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
   //   process.exit();
   res.setHeader("Content-Type", "text/html");
