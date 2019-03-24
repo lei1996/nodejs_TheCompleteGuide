4b7ec3eaddc18365396f73ff4e382ac989aaed47
lin <lin@lindeAir-2.lan>
2019-03-24 21:55:51 +0800

    修改 js 引入 html 模板。

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+  // 不能直接 引用文件路径， 需要 引入 path
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
