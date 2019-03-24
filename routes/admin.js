8893b3640cec2cc9329c736db9d2732439fd2153
lin <lin@lindeAir-2.lan>
2019-03-24 21:42:14 +0800

    修改一个小细节

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   console.log("In another middleware!");
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product!</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add product!</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   // 这里会 显示undefined, 要引入 bodyParser 这个插件
   console.log(req.body);
   res.redirect("/");
