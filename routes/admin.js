3c710b54a56f113a1afb8edc8d86cd5767a2bded
lin <lin@lindeAir-2.lan>
2019-04-01 00:13:26 +0800

    12

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,28 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../utils/path");
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
   });
 });
 
-router.post("/add-product", (req, res, next) => {
-  // 这里会 显示undefined, 要引入 bodyParser 这个插件
-  console.log(req.body);
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.redirect('/');
 });
 
 exports.routes = router;
--- utils/path.js
+++
@@ -1,4 +0,0 @@
-const path = require("path");
-
-// 获取当前脚本的路径
-module.exports = path.dirname(process.mainModule.filename);
